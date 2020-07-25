59507593d0eddac4f5b02878dad11c5d3cadf380
Manh <parabol123654@gmail.com>
2020-07-26 00:37:24 +0700

    first commit

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+# Create your models here.
+class PulishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PulishedManager,self).get_queryset().filter(status='published')
+class Post(models.Model):
+    STATUS_CHOICES=(
+        ('draft','Draft'),
+        ('published','Pulished'),
+    )
+    title=models.CharField(max_length=250)
+    slug=models.SlugField(max_length=250,unique_for_date='publish')
+    author=models.ForeignKey(User,on_delete=models.CASCADE,related_name='blog_posts')
+    body=models.TextField()
+    publish=models.DateTimeField(default=timezone.now)
+    created=models.DateTimeField(auto_now_add=True)
+    updated=models.DateTimeField(auto_now=True)
+    status=models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
+    objects=models.Manager()
+    published=PulishedManager()
+
+    class Meta:
+        ordering=('-publish',)# the Meta class inside the model contains metadata ==> sort results by the publish field in descending order
+    def __str__(self):
+        return self.title
+    # Canonical URLs for models
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',args=[self.publish.year,self.publish.month,self.publish.day, self.slug])
